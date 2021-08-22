cb6ffc351a6f03a970bc59bda3442b3fcb08cd2d
Lukas Jenks <lukasjenks@gmail.com>
2021-08-22 11:38:36 -0600

    Finish basic player controller and camera controller

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    [SerializeField]
+    private float xOffset;
+    [SerializeField]
+    private float yOffset;
+    [SerializeField]
+    private float zOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        playerPos.x = playerPos.x - xOffset;
+        playerPos.y = playerPos.y - yOffset;
+        playerPos.z = playerPos.z - zOffset;
+        // adjust camera pos with calc'ed vals
+        transform.position = playerPos;
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# brackeys-game-jam-2021-2
